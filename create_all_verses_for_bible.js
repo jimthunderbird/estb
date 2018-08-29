ac2c909125895bb57f72b17ce99ef54091db6f79
jim zike huang <jim.javathunderbird@gmail.com>
2018-08-28 21:21:28 -0700

    better memory management

--- create_all_verses_for_bible.js
+++ create_all_verses_for_bible.js
@@ -8,6 +8,43 @@ function sleep(ms){
   })
 }
 
+const processEntry = async (entry) => {
+  for (let i = 1; i <= entry.chapterCount; i ++) {
+    const url = `https://www.studylight.org/commentary/${entry.book}/${i}.html`;
+    const response = await axios.get(url);
+    const dom = new JSDOM(response.data);
+    const links = dom.window.document.querySelectorAll('a');
+    const verseCount = Math.max(...
+      Object.values(links)
+      .map(entry => entry.textContent)
+      .filter(entry => entry.substring(0,6) === 'Verse ')
+      .map(entry => parseInt(entry.replace('Verse', '')))
+    );
+
+    if (typeof verseCount === 'number') {
+      for (let vc = 1; vc <= verseCount; vc ++) {
+        const verseFile = `data/${entry.book}/${i}/${vc}.txt`;
+        if (fs.existsSync(verseFile)) {
+          continue;
+        }
+        execSync(`mkdir -p data/${entry.book}/${i}`);
+        const verseUrl = `https://www.studylight.org/commentary/${entry.book}/${i}-${vc}.html`;
+        axios.get(verseUrl).then(response => {
+          const dom = new JSDOM(response.data);
+          const title = dom.window.document.querySelector(".extra_title").textContent;
+          const body = dom.window.document.querySelector(".subtitle").textContent;
+          fs.writeFileSync(verseFile, title + "\n" + body);
+          console.log(`Done creating verse ${entry.book}/${i}/${vc}`);
+        }).catch(error => {
+          // handle error
+          fs.appendFileSync(logFile, verseUrl + "\n");
+          console.log(error);
+        });
+      }
+    }
+  }
+};
+
 const fs = require('fs');
 const axios = require('axios');
 const jsdom = require('jsdom');
@@ -19,56 +56,21 @@ const logFile = './pending-verse-urls.log';
 //first, truncate the log
 fs.writeFileSync(logFile, '');
 
-const booksChapterCounts = fs.readFileSync('./bible.books.chapter.count').toString().trim()
-  .split("\n")
-  .map(line => {
-    const comps = line.split(':');
-    return {
-      book: comps[0].trim(),
-      chapterCount: parseInt(comps[1].trim())
-    }
-  }
-  );
-
-booksChapterCounts.forEach(entry => {
-  for (let i = 1; i <= entry.chapterCount; i ++) {
-    const url = `https://www.studylight.org/commentary/${entry.book}/${i}.html`;
-    (async () => {
-      try {
-        const response = await axios.get(url);
-        const dom = new JSDOM(response.data);
-        const links = dom.window.document.querySelectorAll('a');
-        const verseCount = Math.max(...
-          Object.values(links)
-          .map(entry => entry.textContent)
-          .filter(entry => entry.substring(0,6) === 'Verse ')
-          .map(entry => parseInt(entry.replace('Verse', '')))
-        );
-
-        if (typeof verseCount === 'number') {
-          for (let vc = 1; vc <= verseCount; vc ++) {
-            const verseFile = `data/${entry.book}/${i}/${vc}.txt`;
-            if (fs.existsSync(verseFile)) {
-              continue;
-            }
-            execSync(`mkdir -p data/${entry.book}/${i}`);
-            const verseUrl = `https://www.studylight.org/commentary/${entry.book}/${i}-${vc}.html`;
-            axios.get(verseUrl).then(response => {
-              const dom = new JSDOM(response.data);
-              const title = dom.window.document.querySelector(".extra_title").textContent;
-              const body = dom.window.document.querySelector(".subtitle").textContent;
-              fs.writeFileSync(verseFile, title + "\n" + body);
-              console.log(`Done creating verse ${entry.book}/${i}/${vc}`);
-            }).catch(error => {
-              // handle error
-              fs.appendFileSync(logFile, verseUrl + "\n");
-              console.log(error);
-            });
-          }
+(async () => {
+  try {
+    const booksChapterCounts = fs.readFileSync('./bible.books.chapter.count').toString().trim()
+      .split("\n")
+      .map(line => {
+        const comps = line.split(':');
+        return {
+          book: comps[0].trim(),
+          chapterCount: parseInt(comps[1].trim())
         }
-      } catch(err) {
-        console.log(`error request for ${entry.book}: ${i}: ${verseCount}`);
       }
-    })();
+      );
+
+    booksChapterCounts.forEach(entry => processEntry(entry));
+  } catch(err) {
+    console.log(err);
   }
-});
+})();
